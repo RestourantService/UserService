7b12121d3b887b023c0d1fc73410572bf38a6d0e
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-07-08 12:52:09 +0500

    changes

---
+++ storage/postgres/postgres.go
@@ -0,0 +1,26 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	cfg := config.Load()
+
+	conn := fmt.Sprintf("port = %s host=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DB_PORT, cfg.DB_HOST, cfg.DB_USER, cfg.DB_PASSWORD, cfg.DB_NAME)
+
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
