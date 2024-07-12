3d0e06b675bcbc9564b7edf342f97d8cc171b968
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-07-12 19:30:12 +0500

    Abbos etdila shunga push qivomman

--- api/router.go
+++ api/router.go
@@ -1,16 +1,24 @@
 package api
 
 import (
-	"user_service/api/handler"
-
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "user_service/api/docs"
+	"user_service/api/handler"
 )
 
+// @title Authorazation
+// @version 1.0
+// @description API Gateway of Authorazation
+// @host localhost:8085
+// BasePath: /
 func Router(hand *handler.Handler) *gin.Engine {
 	router := gin.Default()
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	user := router.Group("/user")
 	user.POST("/register", hand.Register)
 	user.POST("/login", hand.Login)
-	user.GET("/refresh", hand.RefreshToken)
+	user.POST("/refresh", hand.RefreshToken)
 	return router
 }
